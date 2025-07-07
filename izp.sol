5da0234c504b0cc7d26d637b0de9401bdd38e3e5
Lucas Roscoe <lucascardosoroscoe@gmail.com>
2025-07-07 19:52:38 -0400

    Innitial Code

---
+++ izp.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract IZPToken is ERC20, Ownable {
+    uint256 public constant MAX_SUPPLY = 5_000_000 * 10**18;
+
+    event TokensBurned(uint256 amount);
+    event BuybackExecuted(uint256 amount);
+
+    constructor() ERC20("IngressoZapp Token", "IZP") {
+        _mint(msg.sender, MAX_SUPPLY);
+    }
+
+    /// @notice Permite que o dono do contrato queime tokens mantidos pelo próprio contrato
+    function burn(uint256 amount) external onlyOwner {
+        _burn(address(this), amount);
+        emit TokensBurned(amount);
+    }
+
+    /// @notice Simula a recompra e queima dos tokens usando saldo disponível no contrato
+    function simulateBuyback(uint256 amount) external onlyOwner {
+        require(balanceOf(address(this)) >= amount, "Saldo insuficiente no contrato");
+        _burn(address(this), amount);
+        emit BuybackExecuted(amount);
+    }
+}
